2d52a605b707e5fa609b5167be05073c955e2b79
Logan Harrington <lharrington@forged-tech.com>
2025-10-01 19:22:12 -0500

    Added test method to home controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,14 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function getindex(): string
     {
         return view('welcome_message');
+    }
+    public function getTest($echo = 'Test String Empty')
+    {
+        return $this->response->setJSON([
+            'Your test string' => $echo
+        ]);
     }
 }
